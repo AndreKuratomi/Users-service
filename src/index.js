9b6c2f73c3b9aff46ff8e6ff4fb9de878a81b706
André Kuratomi <andrekuratomi@gmail.com>
2022-01-15 21:17:20 -0300

    feat: yups drafted

--- src/index.js
+++ src/index.js
@@ -6,6 +6,33 @@ import { v1 as uuidv1, v4 as uuidv4, v5 as uuidv5 } from "uuid";
 const app = express();
 app.use(express.json());
 
+// ==================YUPS====================
+const registerSchema = yup.object().shape({
+  id: yup
+    .string()
+    .notRequired()
+    .default(function () {
+      return uuidv4();
+    }),
+  username: yup.string().required(),
+  age: yup.number().positive().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+  createdOn: yup
+    .string()
+    .notRequired()
+    .default(function () {
+      return new Date();
+    }),
+});
+
+const loginSchema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required(),
+});
+
+// ==================MIDDLEWARES====================
+
 app.listen(3000, () => {
   console.log("Running at port 'http://localhost:3000'");
 });
